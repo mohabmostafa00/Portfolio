bc31b4435ee72ddd5311ef8052d09092fbeedd5e
Mohab <hobamostafa00@gmail.com>
2026-05-16 00:29:12 -0700

    update project

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,17 +1,52 @@
 import "./style.css";
-import Particle from "../Particle/Particle";
-// import { PropagateLoader} from "react-spinners";
+import { motion } from "framer-motion";
+import { HashLoader } from "react-spinners";
 
-function Loading() {
+function LoadingScreen() {
   return (
-    <div className="loading-screen">
-      <Particle />
-      <h1 className="loading-title">Mohab Mostafa</h1>
-      <div className="loading-bar"></div>
-      {/* <PropagateLoader color="#00eeff" size={30}/> */}
-      <p className="loading-sub">Loading Portfolio...</p>
-    </div>
+    <motion.div
+      className="loading-screen"
+      initial={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.8 }}
+    >
+      <div className="background-glow"></div>
+
+      <motion.h1
+        className="loading-logo"
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 1.6,
+        }}
+      >
+        Mohab<span>.</span>
+      </motion.h1>
+
+      <motion.p
+        className="loading-text"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.3,
+          duration: 1.5,
+        }}
+      >
+        Building Modern Web Experiences{" "}
+      </motion.p>
+
+      <motion.div
+        initial={{ scale: 0.7, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          delay: 0.5,
+          duration: 1.3,
+        }}
+      >
+        <HashLoader className="loader" color="#00eeff" size={45} />
+      </motion.div>
+    </motion.div>
   );
 }
 
-export default Loading;
+export default LoadingScreen;
